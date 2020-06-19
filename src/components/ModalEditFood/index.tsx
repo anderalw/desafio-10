f06fd108ffc06ba410280b51c9edc829153c8275
Anderson L. Wiedemann <anderalw@gmail.com>
2020-06-19 12:20:41 -0300

    desafio

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -39,9 +39,15 @@ const ModalEditFood: React.FC<IModalProps> = ({
 
   const handleSubmit = useCallback(
     async (data: IEditFoodData) => {
-      // EDIT A FOOD PLATE AND CLOSE THE MODAL
+      const { name, image, price, description } = data;
+      handleUpdateFood({
+        name,
+        image,
+        price,
+        description,
+      });
     },
-    [handleUpdateFood, setIsOpen],
+    [handleUpdateFood],
   );
 
   return (
